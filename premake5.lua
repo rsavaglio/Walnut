94eca51f6467ceb07a53c3dae2ec71611fe7aae0
Rob Savaglio <robert.savaglio@ontariotechu.net>
2020-06-03 09:15:14 -0400

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,112 @@
+workspace "Walnut"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{architechture}"
+
+project "Walnut"
+	location "Walnut"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"WN_PLATFORM_WINDOWS",
+			"WN_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "WN_DEBUG"
+		symbols "On"
+
+	
+	filter "configurations:Release"
+		defines "WN_RELEASE"
+		optimize "On"
+
+	
+	filter "configurations:Dist"
+		defines "WN_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Walnut/vendor/spdlog/include",
+		"Walnut/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"WN_PLATFORM_WINDOWS"
+		}
+
+	links
+	{
+		"Walnut"
+	}
+
+	filter "configurations:Debug"
+		defines "WN_DEBUG"
+		symbols "On"
+
+	
+	filter "configurations:Release"
+		defines "WN_RELEASE"
+		optimize "On"
+
+	
+	filter "configurations:Dist"
+		defines "WN_DIST"
+		optimize "On"
