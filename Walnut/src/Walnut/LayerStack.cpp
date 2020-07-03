3a4c3ebf3c51712ad8899eae4cb08c12c9658c14
Rob Savaglio <robert.savaglio@ontariotechu.net>
2020-07-03 16:32:05 -0400

    OnAttach and OnDetach methods now called indside LayerStack

--- Walnut/src/Walnut/LayerStack.cpp
+++ Walnut/src/Walnut/LayerStack.cpp
@@ -18,11 +18,13 @@ namespace Walnut {
 	{
 		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
 		m_LayerInsertIndex++;
+		layer->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
 	{
 		m_Layers.emplace_back(overlay);
+		overlay->OnAttach();
 	}
 
 	void LayerStack::PopLayer(Layer* layer)
@@ -32,7 +34,9 @@ namespace Walnut {
 		{
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
+			layer->OnDetach();
 		}
+
 	}
 
 	void LayerStack::PopOverlay(Layer* overlay)
@@ -41,6 +45,7 @@ namespace Walnut {
 		if (it != m_Layers.end())
 		{
 			m_Layers.erase(it);
+			overlay->OnDetach();
 		}
 	}
 }
